796d201a1e1017d985b8d00e3abd111ed66043fd
Leandro <leandro.cruz.oliveira@hotmail.com>
2019-02-15 17:11:36 -0200

    Recorde aparecendo na tela

--- Assets/Códigos/movimentacao.cs
+++ Assets/Códigos/movimentacao.cs
@@ -11,7 +11,7 @@ public class Movimentacao : MonoBehaviour {
     public int moedas = 0;
     public TextMesh total;
 
-    float forca = 20f;
+    float forca = 20;
     public Rigidbody2D player;
     public bool tocachao = false;
 
@@ -19,11 +19,25 @@ public class Movimentacao : MonoBehaviour {
     float chaograu = 0.2f;
     public LayerMask piso;
 
+    public GameObject Largada;
+    public GameObject jogador;
+    private float distancia = 0;
+    private int recorde = 0;
+    public TextMesh total_Distancia;
+
     void Start ()
     {
         player = GetComponent<Rigidbody2D>();
         rigd = gameObject.GetComponent <Rigidbody2D>();
 	}
+
+    private void Update()
+    {
+        distancia = Vector2.Distance(Largada.transform.position, jogador.transform.position);
+        Debug.Log(distancia);
+        recorde = (int)distancia;
+        total_Distancia.text = recorde.ToString();
+    }
 
     void FixedUpdate()               
     {
@@ -36,7 +50,7 @@ public class Movimentacao : MonoBehaviour {
         if (tocachao)                            //PULA com o botão
         {
             player.AddForce(new Vector2(0, forca));
-            VelocidadeAceleração = VelocidadeAceleração * 1.1f;
+            VelocidadeAceleração = VelocidadeAceleração * 1.01f;
         }
     }
 
